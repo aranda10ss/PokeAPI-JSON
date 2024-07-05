9a611886ab4774b22293a8c3d9955649e03ba528
6loccee <martiin17jr@gmail.com>
2024-07-05 11:22:05 +0200

    style: formatted by linter

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require('./src/app')
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+	console.log(`Server is running on port ${port}`)
 })
--- src/app.js
+++ src/app.js
@@ -8,44 +8,44 @@ const jsonFile = path.join(__dirname, '..', 'data/pokemon.json')
 app.use(express.json())
 
 const getPokemons = () => {
-  const data = fs.readFileSync(jsonFile)
-  return JSON.parse(data)
+	const data = fs.readFileSync(jsonFile)
+	return JSON.parse(data)
 }
 
 app.get('/pokemons', (_, res) => {
-  const pokemons = getPokemons()
-  res.json(pokemons)
+	const pokemons = getPokemons()
+	res.json(pokemons)
 })
 
 app.get('/pokemons/:id', (req, res) => {
-  const pokemons = getPokemons()
-  const pokemonId = parseInt(req.params.id, 10)
-  const pokemon = pokemons.find(p => p.id === pokemonId)
+	const pokemons = getPokemons()
+	const pokemonId = Number.parseInt(req.params.id, 10)
+	const pokemon = pokemons.find(p => p.id === pokemonId)
 
-  if (!pokemon) {
-    return res.status(404).json({ error: 'pokemont not found'})
-  }
+	if (!pokemon) {
+		return res.status(404).json({ error: 'pokemont not found' })
+	}
 
-  res.json(pokemon)
+	res.json(pokemon)
 })
 
 app.post('/pokemons', (req, res) => {
-  try {
-    const pokemons = getPokemons()
-    const newPokemon = { id: pokemons.length + 1, ...req.body }
-    
-    if(!newPokemon.id || !newPokemon.name) {
-      return res.status(400).json({ error: 'Invalid request' })
-    }
+	try {
+		const pokemons = getPokemons()
+		const newPokemon = { id: pokemons.length + 1, ...req.body }
 
-    pokemons.push(newPokemon)
-  
-    fs.writeFileSync(jsonFile, JSON.stringify(pokemons, null, 2))
-  
-    res.status(201).json(newPokemon)
-  } catch (err) {
-    res.status(500).json({ error: err.message })
-  }
+		if (!newPokemon.id || !newPokemon.name) {
+			return res.status(400).json({ error: 'Invalid request' })
+		}
+
+		pokemons.push(newPokemon)
+
+		fs.writeFileSync(jsonFile, JSON.stringify(pokemons, null, 2))
+
+		res.status(201).json(newPokemon)
+	} catch (err) {
+		res.status(500).json({ error: err.message })
+	}
 })
 
 module.exports = app
